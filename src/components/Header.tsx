ff2bed096ac1477f446782c4844a44641077f7bb
Goran SD <g.cvetic23@gmail.com>
2024-03-26 12:23:00 -0700

    Fix import casing in layout.tsx and add navigation links in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,12 @@
+import Link from "next/link";
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Link href="/">Home</Link>
+      <Link href="/events/all">All Events</Link>
+    </header>
+  );
 };
 
 export default Header;
