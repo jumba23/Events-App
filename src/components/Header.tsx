cf47fed821453bd6a5141f7578a429af04896238
Goran SD <g.cvetic23@gmail.com>
2024-03-27 12:16:21 -0700

    Refactor header navigation to use dynamic routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,25 @@
 import Link from "next/link";
 import Logo from "./logo";
 
+const routes = [
+  { name: "Home", path: "/" },
+  { name: "All Events", path: "/events/all" },
+];
+
 const Header = () => {
   return (
     <header className="flex justify-between items-center border-b border-white/10 h-14 px-3 md:px-9">
       <Logo />
       <nav>
         <ul className="flex gap-x-6 text-sm">
-          <li>
-            <Link href="/events/austin">Austin</Link>
-          </li>
-          <li>
-            <Link href="/events/seattle">Seattle</Link>
-          </li>
+          {routes.map((route) => (
+            <li
+              className="text-white/50 hover:text-white transition"
+              key={route.path}
+            >
+              <Link href={route.path}>{route.name}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
       <Link href="/">Home</Link>
