54e7d0abdbbba890f17c6f77d4acf891011d1020
Goran SD <g.cvetic23@gmail.com>
2024-03-27 11:56:41 -0700

    Update layout and add search form to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <main>Home Page</main>;
+  return (
+    <main>
+      <h1>Find events around you</h1>
+      <p>Browse more than 10,000 events around you</p>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Enter your location"
+          spellCheck={false}
+        />
+        <button>Search</button>
+      </form>
+    </main>
+  );
 }
