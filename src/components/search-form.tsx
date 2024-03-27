0c9a59fe024e387227c01e405ac1b807c72328f4
Goran SD <g.cvetic23@gmail.com>
2024-03-27 14:16:33 -0700

    Add search functionality to SearchForm component

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,6 +1,10 @@
 "use client";
 
+import { useState } from "react";
+
 const SearchForm = () => {
+  const [searchText, setSearchText] = useState("");
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("Form submitted");
@@ -8,6 +12,8 @@ const SearchForm = () => {
   return (
     <form onSubmit={handleSubmit} className="w-full sm:w-[580px]">
       <input
+        value={searchText}
+        onChange={(e) => setSearchText(e.currentTarget.value)}
         className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition hover:ring-2 focus:ring-2 focus:bg-white/10"
         placeholder="Enter your location"
         spellCheck={false}
