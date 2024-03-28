0f88165b2a3c0acb0acb93b450234f08b7db5e4d
Goran SD <g.cvetic23@gmail.com>
2024-03-28 13:38:08 -0700

    Add image support to EventCard component

--- src/components/event-card.tsx
+++ src/components/event-card.tsx
@@ -1,11 +1,27 @@
 import { EventoEvent } from "@/lib/types";
+import Image from "next/image";
 
 type EventCardProps = {
   event: EventoEvent;
 };
 
 const EventCard = ({ event }: EventCardProps) => {
-  return <section>{event.name}</section>;
+  return (
+    <section className="flex flex-col flex-1 basis-80 h-[380px] max-w-[500px] bg-white/[5%] rounded-xl overflow-hidden">
+      <Image
+        src={event.imageUrl}
+        alt={event.name}
+        width={500}
+        height={280}
+        className="h-[60%] object-fit"
+      />
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <h2 className="text-2xl font-semibold">{event.name}</h2>
+        <p className="italic text-white/75">By {event.organizerName}</p>
+        <p className="text-sm text-white/50 mt-4">{event.location}</p>
+      </div>
+    </section>
+  );
 };
 
 export default EventCard;
--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -7,7 +7,7 @@ type EventListProps = {
 
 const EventsList = ({ events }: EventListProps) => {
   return (
-    <section>
+    <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
       {events.map((event) => (
         <EventCard key={event.id} event={event} />
       ))}
