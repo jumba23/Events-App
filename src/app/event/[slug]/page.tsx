fd462659a54d13d503b33b193075169c781163e7
Goran SD <g.cvetic23@gmail.com>
2024-03-29 10:20:09 -0700

    Refactor button styling and add state effects

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -53,7 +53,7 @@ const EventPage = async ({ params }: EventPageProps) => {
             <p className="whitespace-nowrap text-xl text-white/75">
               Organized by <span className="italic">{event.organizerName}</span>
             </p>
-            <button className="bg-white/20 bg-blur text-lg capitalize mt-5 lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2 hover:scale-105 active:scale-[1.02] transition focus:scale-105">
+            <button className="bg-white/20 bg-blur text-lg capitalize mt-5 lg:mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2 state-effects">
               Get Tickets
             </button>
           </div>
