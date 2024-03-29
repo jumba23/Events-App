96c04bdfbcd660a63c66f058389577ffc1b0971d
Goran SD <g.cvetic23@gmail.com>
2024-03-29 11:48:33 -0700

    Refactor skeleton component and loading screen

--- src/app/event/[slug]/loading.tsx
+++ src/app/event/[slug]/loading.tsx
@@ -2,10 +2,10 @@ import Skeleton from "@/components/skeleton";
 
 const Loading = () => {
   return (
-    <div className="flex flex-col items-center gap-y-4 pt-28">
-      <Skeleton />
-      <Skeleton />
-      <Skeleton />
+    <div className="animate-pulse flex flex-col items-center gap-y-4 pt-28">
+      <Skeleton className="h-4 w-[550px]" />
+      <Skeleton className="h-4 w-[400px]" />
+      <Skeleton className="h-4 w-[430px]" />
     </div>
   );
 };
--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,5 +1,13 @@
-const Skeleton = () => {
-  return <div className="h-4 w-[550px] rounded-md bg-white/5" />;
+import { cn } from "@/lib/utils";
+
+type SkeletonProps = {
+  className?: string;
+};
+
+const Skeleton = ({ className }: SkeletonProps) => {
+  return (
+    <div className={cn("h-4 w-[550px] rounded-md bg-white/5", className)} />
+  );
 };
 
 export default Skeleton;
