9a6a338d3cf98ab50bea4b94d58300b013c1634b
Goran SD <g.cvetic23@gmail.com>
2024-03-29 08:48:50 -0700

    Update API endpoint to include city parameter

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -12,7 +12,7 @@ const EventsPage = async ({ params }: EventsPageProps) => {
   const city = params.city;
 
   const response = await fetch(
-    "https://bytegrad.com/course-assets/projects/evento/api/events"
+    `https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`
   );
 
   const events: EventoEvent[] = await response.json();
