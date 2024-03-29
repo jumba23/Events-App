30c6b19f5a5eee95f6accf2bb9b0dfcb3f2cb486
Goran SD <g.cvetic23@gmail.com>
2024-03-29 10:03:18 -0700

    Add H1 component and update event page layout

--- src/app/event/[slug]/page.tsx
+++ src/app/event/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import H1 from "@/components/h1";
 import Image from "next/image";
 
 type EventPageProps = {
@@ -18,7 +19,7 @@ const EventPage = async ({ params }: EventPageProps) => {
 
   return (
     <main>
-      <section className="relative h-[361px] overflow-hidden">
+      <section className="relative  overflow-hidden flex justify-center items-center py-14 md:py-20">
         <Image
           src={event.imageUrl}
           alt="Event background image"
@@ -28,13 +29,34 @@ const EventPage = async ({ params }: EventPageProps) => {
           className="object-cover blur-3xl z-0"
           priority
         />
-        <div className="z-1 relative">
+        <div className="z-1 relative flex flex-col lg:flex-row gap-6 lg:gap-16">
           <Image
             src={event.imageUrl}
             alt={event.name}
             width={300}
             height={201}
+            className="rounded-xl border-2 border-white/50 object-cover"
           />
+          <div className="flex flex-col">
+            <p className="text-white/75">
+              {/* display date in day name comma name of the month and then day number of month */}
+              {/* example: Friday, July 12 */}
+              {new Date(event.date).toLocaleDateString("en-US", {
+                weekday: "long",
+                month: "long",
+                day: "numeric",
+              })}
+            </p>
+            <H1 className="mb-2 mt-1 whitespace-nowrap lg:text-5xl">
+              {event.name}
+            </H1>
+            <p className="whitespace-nowrap text-xl text-white/75">
+              Organized by <span className="italic">{event.organizerName}</span>
+            </p>
+            <button className="bg-white/20 bg-blur text-lg capitalize mt-auto w-[95vw] rounded-md border-white/10 border-2 sm:w-full py-2">
+              Get Tickets
+            </button>
+          </div>
         </div>
       </section>
     </main>
