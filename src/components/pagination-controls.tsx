fbb8bffd1339a5bcceee1581cf5e7665fddf0fce
Goran SD <g.cvetic23@gmail.com>
2024-03-30 15:23:24 -0700

    Refactor pagination controls styling

--- src/components/pagination-controls.tsx
+++ src/components/pagination-controls.tsx
@@ -1,20 +1,17 @@
 import { ArrowLeftIcon, ArrowRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
+const btnStyles =
+  "flex items-center gap-x-2 text-white px-5 py-3 bg-white/5 rounded-md opacity-75 hover:opacity-100 transition text-sm";
+
 const PaginationControls = () => {
   return (
-    <section className="flex">
-      <Link
-        href="/events/austin?page=1"
-        className="flex text-white px-5 py-3 bg-white/5 rounded-md opacity-75"
-      >
+    <section className="flex justify-between w-full">
+      <Link href="/events/austin?page=1" className={btnStyles}>
         <ArrowLeftIcon />
         Previous
       </Link>
-      <Link
-        href="/events/austin?page=3"
-        className="text-white px-5 py-3 bg-white/5"
-      >
+      <Link href="/events/austin?page=3" className={btnStyles}>
         Next
         <ArrowRightIcon />
       </Link>
